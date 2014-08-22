dab80e3386df6b0fb545deebeadcd8831b781a7d
Stan Low <a22301613@yahoo.com.tw>
2014-08-22 18:24:09 +0800

    testing AAE config

--- config/initializers/active_admin_editor.rb
+++ config/initializers/active_admin_editor.rb
@@ -1,5 +1,5 @@
 ActiveAdmin::Editor.configure do |config|
-  config.s3_bucket = ENV["AWS_BUCKET"] || Rails.application.secrets.s3_bucket
-  config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"] || Rails.application.secrets.aws_access_key_id
-  config.aws_access_secret = ENV["AWS_SECRET_ACCESS_KEY"] || Rails.application.secrets.aws_access_secret
+  config.s3_bucket = ENV["AWS_BUCKET"] || "123"
+  config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"] || "234"
+  config.aws_access_secret = ENV["AWS_SECRET_ACCESS_KEY"] || "234"
 end
