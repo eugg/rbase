9150a34ecbbba37115ea5367f110821e02e0eac5
Stan Low <a22301613@yahoo.com.tw>
2014-08-22 16:32:28 +0800

    set secrets.yml

--- config/initializers/active_admin_editor.rb
+++ config/initializers/active_admin_editor.rb
@@ -1,5 +1,5 @@
 ActiveAdmin::Editor.configure do |config|
-  config.s3_bucket = ENV["AWS_BUCKET"] || "123"
-  config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"] || "123"
-  config.aws_access_secret = ENV["AWS_SECRET_ACCESS_KEY"] || "123"
+  config.s3_bucket = ENV["AWS_BUCKET"] || Rails.application.secrets.s3_bucket
+  config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"] || Rails.application.secrets.aws_access_key_id
+  config.aws_access_secret = ENV["AWS_SECRET_ACCESS_KEY"] || Rails.application.secrets.aws_access_secret
 end
