519298cf975ea06905f73f51fb5f1eb9e4aa8b23
Stan Low <a22301613@yahoo.com.tw>
2014-08-27 12:55:20 +0800

    improved coding style

--- app/models/redactor_rails/picture.rb
+++ app/models/redactor_rails/picture.rb
@@ -1,11 +1,7 @@
 class RedactorRails::Picture < RedactorRails::Asset
-  mount_uploader :data, RedactorRailsPictureUploader, :mount_on => :data_file_name
+  mount_uploader :data, RedactorRailsPictureUploader, mount_on: :data_file_name
 
   def url_content
     url(:content)
-  end
-
-  def extension_white_list
-    %w(jpg jpeg gif png)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RedactorRails::Engine => '/redactor_rails'
+  mount RedactorRails::Engine => "/redactor_rails"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :posts
