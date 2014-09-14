5c1fac820fc44e77cbc703ae9d375b9b0b05c053
Stan Low <a22301613@yahoo.com.tw>
2014-09-14 23:10:17 +0800

    re-add 'bypass: true' to omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,10 +8,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       user_social = UserSocial.find_user_social_by_uid(provider, uid)
       if user_social
         user_social.update_social_data(data)
-        sign_in_and_redirect user_social.user
+        sign_in_and_redirect user_social.user, bypass: true
       elsif user_by_email = User.find_by_email(email)
         user_by_email.create_user_social(data)
-        sign_in_and_redirect user_by_email
+        sign_in_and_redirect user_by_email, bypass: true
       else
         session["devise.social_data"] = data
         redirect_to new_user_session_url, notice: "請登入或註冊"
