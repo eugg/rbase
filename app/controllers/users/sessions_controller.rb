940e488558897f031eb7596550b04ab6554ebf23
Stan Low <a22301613@yahoo.com.tw>
2014-09-14 23:22:27 +0800

    finished 3 social login's spec

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   def new
-    @@user_social_data = {}
     @@user_social_data = session["devise.social_data"]
     super
   end
@@ -9,7 +8,6 @@ class Users::SessionsController < Devise::SessionsController
     super
     if data = @@user_social_data
       resource.create_user_social(data)
-      @@user_social_data = nil
     end
   end
 end
