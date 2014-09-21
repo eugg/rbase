d05c578e79e2e36b079654c532310710681f5491
Stan Low <a22301613@yahoo.com.tw>
2014-09-21 13:41:16 +0800

    improved social login and add email to UserSocial

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,15 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def new
     if session["devise.social_data"]
       session["user_social_data"] = session["devise.social_data"]
-    else
-      session["user_social_data"] = nil
     end
     super
   end
 
   def create
     super
-    if data = session["user_social_data"]
+    if resource.save && data = session["user_social_data"]
       resource.create_user_social(data)
       session["user_social_data"] = nil
     end
