f5adb52a7bd70e824629cb4ea441ad4f9df677bc
Stan Low <a22301613@yahoo.com.tw>
2014-10-22 11:19:58 +0800

    added google_map mode

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,5 +2,5 @@ class Widget < ActiveRecord::Base
   validates :title, presence: true
   validates :order, presence: true
 
-  enum widget_type: [:normal, :facebook]
+  enum widget_type: [:normal, :facebook, :google_map]
 end
